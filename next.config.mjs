5f83113356a2474734e371e1486841b4ffb7855d
Tsukishima1 <1732124764@qq.com>
2024-09-15 19:39:34 +0800

    docs: 修改端口号

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,10 @@ const nextConfig = {
     async rewrites() { 
         return [ 
          //接口请求 前缀带上/api-text/
-            { source: '/api/resume/:path*', destination: `http://localhost:8080/:path*` }, // 意为将/api/resume/xxx 代理到http://localhost:8080/xxx
-            { source: '/api/resume/:path*', destination: `http://localhost:8080/:path*` },
+            { source: '/api/resume/:path*', destination: `http://localhost:30300/:path*` }, // 意为将/api/resume/xxx 代理到http://localhost:8080/xxx
+            { source: '/api/resume/:path*', destination: `http://localhost:30300/:path*` },
         ]
-      },
+    },
 };
 
 export default nextConfig;
